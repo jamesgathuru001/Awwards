ce32b63ba1723435a0a8bdac63711a23629bcdc3
James <jamesgathuru001@gmail.com>
2019-06-30 12:05:20 +0300

    changing configurations to include the database for my project

--- awardsproject/urls.py
+++ awardsproject/urls.py
@@ -1,4 +1,4 @@
-"""awardapp URL Configuration
+"""awardsproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
